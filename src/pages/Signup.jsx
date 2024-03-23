e53b6dd7154f260a3ed76ed525d60260a231eb73
Carolina Rodrigues <rodriguesbcarolina@gmail.com>
2024-03-23 15:03:33 +0000

    changed copy signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -154,7 +154,7 @@ function Signup() {
                           type='submit'
                           className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                         >
-                          Create Account
+                          Create a Client Account
                         </button>
                       </form>
                       {error && <p>{error}</p>}
@@ -249,7 +249,7 @@ function Signup() {
                           type='submit'
                           className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                         >
-                          Sign in
+                          Create a Therapist Account
                         </button>
                       </form>
                       {error && <p>{error}</p>}
