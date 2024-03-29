842e1cba064a01e30dee1f1a7b07e829397eef43
Carolina Rodrigues <rodriguesbcarolina@gmail.com>
2024-03-29 19:14:20 +0000

    added notification for signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import { signup } from '../api/auth.api';
 import { Link, useNavigate } from 'react-router-dom';
 import { Tabs, Tab, Card, CardBody } from '@nextui-org/react';
 import logo from '../assets/logo black svg.svg';
+import { Transition } from '@headlessui/react';
+import { CheckCircleIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon } from '@heroicons/react/20/solid';
 
 function Signup() {
   const [email, setEmail] = useState('');
@@ -13,6 +16,7 @@ function Signup() {
   const [isTherapist, setIsTherapist] = useState(false);
 
   const [error, setError] = useState(null);
+  const [show, setShow] = useState(false);
 
   const navigate = useNavigate();
 
@@ -30,7 +34,15 @@ function Signup() {
       setLastName('');
       setIsTherapist(false);
 
+      setShow(true);
+
+      const timeoutId = setTimeout(() => {
+        setShow(false);
+      }, 5000);
+
       navigate('/login');
+
+      return () => clearTimeout(timeoutId);
     } catch (error) {
       console.log('Error signup', error);
       // this chain gets the error message we defined in the backend
@@ -268,6 +280,55 @@ function Signup() {
               Login
             </Link>
           </p>
+        </div>
+      </div>
+
+      {/* Notifications */}
+      <div
+        aria-live='assertive'
+        className='pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6'
+      >
+        <div className='flex w-full flex-col items-center space-y-4 sm:items-end mt-16'>
+          <Transition
+            show={show}
+            as={Fragment}
+            enter='transform ease-out duration-300 transition'
+            enterFrom='translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2'
+            enterTo='translate-y-0 opacity-100 sm:translate-x-0'
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5'>
+              <div className='p-4'>
+                <div className='flex items-start'>
+                  <div className='flex-shrink-0'>
+                    <CheckCircleIcon
+                      className='h-6 w-6 text-green-400'
+                      aria-hidden='true'
+                    />
+                  </div>
+                  <div className='ml-3 w-0 flex-1 pt-0.5'>
+                    <p className='text-sm font-medium text-gray-900'>
+                      Account successfully created!
+                    </p>
+                  </div>
+                  <div className='ml-4 flex flex-shrink-0'>
+                    <button
+                      type='button'
+                      className='inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                      onClick={() => {
+                        setShow(false);
+                      }}
+                    >
+                      <span className='sr-only'>Close</span>
+                      <XMarkIcon className='h-5 w-5' aria-hidden='true' />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
         </div>
       </div>
     </div>
